26b6bec8076b372040d5c6af69219aaaca67007c
tothzeteny <tothzeteny499@gmail.com>
2024-12-16 15:23:05 +0100

    fortnite

--- frontend/src/pages/Filmek/Filmek.jsx
+++ frontend/src/pages/Filmek/Filmek.jsx
@@ -3,7 +3,7 @@ import './Filmek.css';
 import FelsoNav from '../../components/Navbar/Navbar';
 
 const Filmek = () => {
-    const [filmek, setFilmek] = useState([]);
+    +
 
     useEffect(() => {
         const filmleker = async () => {
@@ -23,26 +23,150 @@ const Filmek = () => {
         filmleker();
     }, []);
 
+    const filmek = [
+        {
+            cim: 'Oppenheimer',
+            kep: '../../public/images/Oppenheimer.jpg',
+            kategoria: 'filmek',
+        },
+        {
+            cim: 'Wall street farkasai',
+            kep: '../../public/images/Wall street farkasai.jpg',
+            kategoria: 'filmek,',
+        },
+        {
+            cim: 'Top Gun',
+            kep: '../../public/images/topgun.jpg',
+            kategoria: 'filmek',
+        },
+        {
+            cim: 'Sötét lovag',
+            kep: '../../public/images/sotetlovag.jpg',
+            kategoria: 'filmek',
+        },
+        {
+            cim: 'Prison Break',
+            kep: '../../public/images/JohnWick.jpg',
+            kategoria: 'filmek',
+        },
+        {
+            cim: 'Deadpool',
+            kep: '../../public/images/deadpool.jpg',
+            kategoria: 'filmek',
+        },
+        {
+            cim: 'Pókverzum',
+            kep: '../../public/images/pokverzum.jpg',
+            kategoria: 'filmek',
+        },
+        {
+            cim: 'Bosszuallok',
+            kep: '../../public/images/Bosszualok.jpg',
+            kategoria: 'filmek',
+        },
+    ];
+
+    const betolt = (index) => {
+        let homeContainer = document.querySelector('.home-container');
+        homeContainer.style.backgroundImage = `url('${filmek[index].kep}')`;
+
+        let sliderInfoImg = document.querySelector('.info-img');
+        let sliderInfoKategoria = document.querySelector('.slider-tipus');
+        let sliderInfoCim = document.querySelector('.slider-title');
+
+        sliderInfoImg.src = filmek[index].kep;
+        sliderInfoKategoria.innerText = filmek[index].kategoria;
+        sliderInfoCim.innerText = filmek[index].cim;
+
+        let thumbImg = document.querySelectorAll('.thumb-img');
+        thumbImg[index].style.border = '3px solid red';
+    };
+
+    const szinez = (index) => {
+        let thumbImg = document.querySelectorAll('.thumb-img');
+        thumbImg[index].style.border = '3px solid orange';
+        thumbImg[index].style.height = '210px';
+        thumbImg[index].style.width = '210px';
+        thumbImg[index].style.transform = 'height width 1s';
+    };
+
+    const torol = (index) => {
+        let thumbImg = document.querySelectorAll('.thumb-img');
+        thumbImg[index].style.border = 'none';
+        thumbImg[index].style.height = '200px';
+        thumbImg[index].style.width = '200px';
+        thumbImg[index].style.transform = 'height width 1s';
+    };
+
     return (
-        <div className="filmek-container">
-            <FelsoNav
-                filmekaktiv={true}
-                sorozatokaktiv={false}
-                loginaktiv={false}
-                registeraktiv={false}
-            />
-            <h1>Dejó</h1>
-            {filmek.map((film) => (
-                <div
-                    className="film-tarto"
-                    key={film._id}
-                >
-                    <h3>{film.cim}</h3>
-                    <img src={film.plakat} />
+        <div className="home-container">
+            <FelsoNav />
+            <div className="slider-container">
+                <div className="slider-info-container">
+                    <div className="infoWrapper">
+                        <div className="slider-info">
+                            <img
+                                className="info-img"
+                                src="../../public/images/deadpool.jpg"
+                                alt=""
+                            />
+                            <div className="slider-raitings">
+                                <p className="slider-raiting">*****</p>
+                                <p className="slider-category">
+                                    <span className="category">Kategória</span>
+                                    <span className="slider-tipus">Filmek</span>
+                                </p>
+                            </div>
+                        </div>
+                        <info className="slider-title">Deadpool</info>
+                    </div>
+                    <button className="slider-btn">Részletek</button>
                 </div>
-            ))}
+                <div className="sliderThumbs-container">
+                    {filmek.map((value, index) => (
+                        <div
+                            className="sliderThumbs"
+                            key={index}
+                        >
+                            <div className="sliderThumbs-img">
+                                <img
+                                    className="thumb-img"
+                                    src={value.kep}
+                                    onClick={() => betolt(index)}
+                                    onMouseEnter={() => szinez(index)}
+                                    onMouseLeave={() => torol(index)}
+                                />
+                            </div>
+                            <div className="thumb-title">{value.cim}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {/* <div className="home-main-container">
+                <h1 id="csetflix">NextStream</h1>
+            </div> */}
         </div>
-    );
+        );
 };
+
+    // return (
+    //     <div className="filmek-container">
+    //         <FelsoNav
+    //             filmekaktiv={true}
+    //             sorozatokaktiv={false}
+    //             loginaktiv={false}
+    //             registeraktiv={false}
+    //         />
+    //         <h1>Dejó</h1>
+    //         {filmek.map((film) => (
+    //             <div
+    //                 className="film-tarto"
+    //                 key={film._id}
+    //             >
+    //                 <h3>{film.cim}</h3>
+    //                 <img src={film.plakat} />
+    //             </div>
+    //         ))}
+    //     </div>
 
 export default Filmek;
