5a5bb5ff6ab21d686a17a71f10c2e435aaaf1d4d
Takigyula <szabo.gergo@szbiszeged.hu>
2025-01-17 15:21:47 +0100

    01.17

--- frontend/src/pages/Sorozatok/Sorozatok.jsx
+++ frontend/src/pages/Sorozatok/Sorozatok.jsx
@@ -1,104 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // import './Home.css';
 import FelsoNav from '../../components/Navbar/Navbar';
 // import Imagesslider from '../../components/Imagesslider/Imagesslider';
 
-const Home = () => {
-    //   // Filmek adatainak szimulálása
-    //   const movies = {
-    //     trending: [
-    //       { title: 'Movie 1', imgUrl: 'https://i.cdn29.hu/apix_collect_c/ngg_images/2306/19/a_vajak_the_witcher_175036_1_original_560x313_cover.jpg' },
-    //       { title: 'Movie 2', imgUrl: 'https://www.justwatch.com/images/poster/301589704/s718/from.jpg' },
-    //       { title: 'Movie 3', imgUrl: 'https://upload.wikimedia.org/wikipedia/hu/6/68/Fortune_hadm%C5%B1velet.jpg' },
-    //       { title: 'Movie 4', imgUrl: 'https://m.media-amazon.com/images/S/pv-target-images/15e19b85511c34f3b2b3dc40f6fe7d62d553f955cf48bc6043dbe2439cd5621a.jpg' },
-    //       { title: 'Movie 5', imgUrl: 'https://www.mafab.hu/static/2023/215/14/274034_1691152718.4816.jpg' },
-    //       { title: 'Movie 6', imgUrl: 'https://media.themoviedb.org/t/p/w500/oKqNNTAMFlX6FwaxL6V7qV27ooG.jpg' },
-    //       { title: 'Movie 7', imgUrl: 'https://m.media-amazon.com/images/M/MV5BOWJhYjdjNWEtMWFmNC00ZjNkLThlZGEtN2NkM2U3NTVmMjZkXkEyXkFqcGc@._V1_.jpg' },
-    //       { title: 'Movie 8', imgUrl: 'https://resizing.flixster.com/suPMbIFPaMa4VWAM72eq76mjJ9g=/ems.cHJkLWVtcy1hc3NldHMvdHZzZXJpZXMvYjRhYjdkZDQtZGE5OC00NTdmLWFiM2QtZTEzNDJcM2RkMGM1LmpwZw==' }
-    //     ],
-    //     topRated: [
-    //       { title: 'Top Movie 1', imgUrl: 'https://m.media-amazon.com/images/M/MV5BYzM3ZTFmMTUtODAxNy00N2IxLWI2MGMtYTdmMDFhNmZkMDI3XkEyXkFqcGc@._V1_.jpg' },
-    //       { title: 'Top Movie 2', imgUrl: 'https://img2.filmek.hu/original/roXSRnno9M6zDQIs1QAWx5F5KDR.jpg' },
-    //       { title: 'Top Movie 3', imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR20iFsO9v7uRw5PfW_UMwGczKRvZ8G-n_zIw&s' },
-    //       { title: 'Top Movie 4', imgUrl: 'https://lh5.googleusercontent.com/proxy/evyCd70ZKzDeHbgkoOBeZzVIfZ8qYwwEM-RxY4FC2aMo1OEpEUJsIveGUqylRd2rm1PI15Mvch8GaERxLGTfVwvxnQiweNSGc19BURuOovfi1AE' },
-    //       { title: 'Top Movie 5', imgUrl: 'https://media.port.hu/images/001/152/122.jpg' },
-    //       { title: 'Top Movie 6', imgUrl: 'https://filmtett.ro/resized/2023/04/filmsidebar_papa-ordoguzo-plakat.jpg' },
-    //       { title: 'Top Movie 7', imgUrl: 'https://dnm.nflximg.net/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABaSWW48t0IluMnNMUonTWQgWcAlj8wyBOu2HYjiNTyy406d8vAGtfcUMq2M0qMGVSng0NUQQn5rP4QCE_ElHCt6M3F62W05Ksc7-Mo1OGv4rcu1vGBk9HaL5HPUR-AVB8MZXVg.jpg?r=488' },
-    //     ],
-    //     action: [
-    //       { title: 'Action Movie 1', imgUrl: 'https://media.port.hu/images/001/569/360.webp' },
-    //       { title: 'Action Movie 2', imgUrl: 'https://www.mafab.hu/static/2023/162/10/515811_1686557043.5039.jpg' },
-    //       { title: 'Action Movie 3', imgUrl: 'https://m.media-amazon.com/images/S/pv-target-images/d67c22b7666dbb6b8b041c6f28d9f7184f5e0c905bd223088b029f3f2bf0c5a6.jpg' },
-    //       { title: 'Action Movie 4', imgUrl: 'https://media.port.hu/images/001/586/066.webp' },
-    //       { title: 'Action Movie 5', imgUrl: 'https://i.ytimg.com/vi/yUirUsoXHuA/hqdefault.jpg' },
-    //     ],
-    //     comedy: [
-    //       { title: 'Comedy Movie 1', imgUrl: 'https://www.panoramicvillas.com/blog/wp-content/uploads/2013/07/dumb-and-dumber-film.jpg' },
-    //       { title: 'Comedy Movie 2', imgUrl: 'https://snitt.hu/system/covers/big/covers_54655.jpg?1617134531' },
-    //       { title: 'Comedy Movie 3', imgUrl: 'https://www.mafab.hu/static/2018/93/10/294366_1522830666.7375.jpg' },
-    //       { title: 'Comedy Movie 4', imgUrl: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8371978_p_v10_aj.jpg' },
-    //       { title: 'Comedy Movie 5', imgUrl: 'https://www.mafab.hu/static/2019/167/10/316119_1560760422.4.jpg' },
-    //     ]
-    //   };
+const Sorozatok = () => {
+    const [sorozatok, setSorozatok] = useState([]);
+    useEffect(() => {
+        const sorozatleker = async () => {
+            const response = await fetch(
+                'http://localhost:3500/api/cinema/sorozatok/series'
+            );
 
-    const filmek = [
-        {
-            cim: 'Totál Szívás',
-            kep: '/images/brakingbad.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Narcos',
-            kep: '/images/Narcos.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Fehér Hó',
-            kep: '/images/Snowfall.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Top Boy',
-            kep: '/images/TopBoy.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Prison Break',
-            kep: '/images/PrisonBreak.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Peaky Blinders',
-            kep: '/images/peakyblinders.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Vikings',
-            kep: '/images/Vikings.jpg',
-            kategoria: 'sorozatok',
-        },
-        {
-            cim: 'Csernobil',
-            kep: '/images/chernobyl.jpg',
-            kategoria: 'sorozatok',
-        },
-    ];
+            if (response.ok) {
+                let result = await response.json();
+                console.log(result.sorozatok);
 
+                setSorozatok(result.sorozatok);
+            }
+        };
+
+        sorozatleker();
+    }, []);
+    
     const betolt = (index) => {
-        let homeContainer = document.querySelector('.home-container');
-        homeContainer.style.backgroundImage = `url('${filmek[index].kep}')`;
+        let homeContainer = document.querySelector('.sorozat-home-container');
+        homeContainer.style.backgroundImage = `url('/images/${sorozatok[index].plakat}')`;
 
         let sliderInfoImg = document.querySelector('.info-img');
         let sliderInfoKategoria = document.querySelector('.slider-tipus');
         let sliderInfoCim = document.querySelector('.slider-title');
 
-        sliderInfoImg.src = filmek[index].kep;
-        sliderInfoKategoria.innerText = filmek[index].kategoria;
-        sliderInfoCim.innerText = filmek[index].cim;
+        sliderInfoImg.src = `/public/images/${sorozatok[index].plakat}`;
+        sliderInfoKategoria.innerText = sorozatok[index].kategoria;
+        sliderInfoCim.innerText = sorozatok[index].cim;
 
         let thumbImg = document.querySelectorAll('.thumb-img');
         thumbImg[index].style.border = '3px solid red';
     };
+    // {
+    //     let thumbImg = document.querySelectorAll('.thumb-img');
+    //     thumbImg[index].style.border = '3px solid orange';
+    //     thumbImg[index].style.height = '210px';
+    //     thumbImg[index].style.width = '210px';
+    //     thumbImg[index].style.transform = 'height width 1s';
+    // };
 
     const szinez = (index) => {
         let thumbImg = document.querySelectorAll('.thumb-img');
@@ -117,7 +62,7 @@ const Home = () => {
     };
 
     return (
-        <div className="home-container">
+        <div className="sorozat-home-container">
             <FelsoNav />
             <div className="slider-container">
                 <div className="slider-info-container">
@@ -125,7 +70,7 @@ const Home = () => {
                         <div className="slider-info">
                             <img
                                 className="info-img"
-                                src="../../public/images/brakingbad.jpg"
+                                src="/public/images/brakingbad.jpg"
                                 alt=""
                             />
                             <div className="slider-raitings">
@@ -143,15 +88,15 @@ const Home = () => {
                     <button className="slider-btn">Részletek</button>
                 </div>
                 <div className="sliderThumbs-container">
-                    {filmek.map((value, index) => (
+                    {sorozatok.map((value, index) => (
                         <div
                             className="sliderThumbs"
                             key={index}
                         >
-                            <div className="sliderThumbs-img">
+                            <div className="sorozat-sliderThumbs-img">
                                 <img
                                     className="thumb-img"
-                                    src={value.kep}
+                                    src={`/images/${value.plakat}`}
                                     onClick={() => betolt(index)}
                                     onMouseEnter={() => szinez(index)}
                                     onMouseLeave={() => torol(index)}
@@ -169,4 +114,4 @@ const Home = () => {
     );
 };
 
-export default Home;
+export default Sorozatok;
--- routes/sorozatokRoutes.js
+++ routes/sorozatokRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getSorozatok } = require('../controllers/sorozatokRouteControllers');
+const { getSorozatok, getSeries } = require('../controllers/sorozatokRouteControllers');
 
 const router = express.Router();
 
 router.get('/', getSorozatok);
+router.get('/series', getSeries);
 
 module.exports = router;
